5b98a23a1fc1be2c865a1d22b2302a041d90ef55
rnwathon <rakhmatnashrul@gmail.com>
2022-05-09 19:30:50 +0700

    chore: set up craco config for antd LESS and tailwind

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,26 @@
 const { ESLINT_MODES } = require('@craco/craco');
 
+const CracoLessPlugin = require('craco-less');
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
+
 module.exports = {
   mode: ESLINT_MODES.file,
+  style: {
+    postcssOptions: {
+      plugins: [autoprefixer, tailwind],
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
 };
