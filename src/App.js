9b60c74f9355a5a7c00cc7e713a53c343e1f0e02
rnwathon <rakhmatnashrul@gmail.com>
2022-05-12 18:09:28 +0700

    feat: configure basic react-router set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div>
-      React AntD Tailwind Starter
-    </div>
-  );
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DashboardLayout } from './Layouts';
+import { Dashboard } from './Pages';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DashboardLayout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
