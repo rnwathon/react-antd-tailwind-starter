e5fdd95b17f591ca05a5673140170fc4ee90e661
rnwathon <rakhmatnashrul@gmail.com>
2022-05-15 00:45:43 +0700

    feat: set up component list route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { DashboardLayout } from './Layouts';
-import { Dashboard } from './Pages';
+import { ComponentList, Dashboard } from './Pages';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
         <Route index element={<Dashboard />} />
+        <Route path="/components" element={<ComponentList />} />
       </Route>
     </Routes>
   </BrowserRouter>
