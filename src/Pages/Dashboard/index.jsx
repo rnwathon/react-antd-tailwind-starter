79f5be116c31928b8a857774bcab62980c9ac9a0
rnwathon <rakhmatnashrul@gmail.com>
2022-05-14 23:28:47 +0700

    feat: update simple dashboard page

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,100 +1,199 @@
 import React from 'react';
+import { Avatar, Card, Col, List, Row, Space, Statistic, Table, Tag } from 'antd';
+import { ArrowDownOutlined, ArrowUpOutlined, LikeOutlined } from '@ant-design/icons';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (tags) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const dataTable = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const dataList = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
 
 const Dashboard = () => (
-  <h1>
-    Dasboard Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ducimus
-    accusamus alias eveniet vitae error sequi ullam unde ipsum repudiandae, aspernatur
-    praesentium cum nam, possimus quo quia non deleniti minima sunt qui vel provident
-    pariatur maiores earum. Animi obcaecati assumenda voluptas officiis vero, consequatur,
-    repudiandae culpa doloribus quia repellat consectetur autem itaque deleniti maiores
-    dolorum reprehenderit! Laborum at libero officiis autem doloribus quis natus
-    cupiditate molestias vero quidem consectetur nulla non explicabo, alias magni, minus
-    nihil quo. Corrupti accusantium est aspernatur assumenda fuga a quia adipisci
-    inventore repellendus voluptas itaque, doloribus vero iusto reprehenderit, cupiditate
-    tempore, rerum atque unde reiciendis maiores? Cumque tempore ad obcaecati ut in
-    blanditiis eligendi magni inventore natus delectus labore adipisci nulla vero alias
-    nesciunt qui ex consequuntur suscipit aliquid quasi, doloremque expedita minus odit.
-    Exercitationem enim est doloribus, architecto eveniet aut repellat impedit, incidunt
-    molestiae voluptatibus, dolorum modi! Quaerat laborum eveniet quasi, atque sunt
-    dolores nihil laudantium quas perferendis tempora distinctio? Incidunt exercitationem
-    quibusdam vel, voluptates vitae maiores dolorum at excepturi officiis accusantium
-    suscipit ex vero, quod, repellendus totam quasi nihil? Laborum qui quaerat autem
-    minima assumenda unde iste maiores ipsum officia fuga architecto, quam sapiente porro
-    repellat soluta laudantium quas, saepe asperiores commodi eos minus nisi enim. Quidem
-    hic sit maxime rem eligendi soluta vitae suscipit temporibus quos enim amet
-    laboriosam, sunt quod eaque, in asperiores voluptates placeat! Id, sint perferendis?
-    Doloribus voluptates maxime nulla facere doloremque ipsa autem, fugit, repellendus
-    dolores eaque reiciendis placeat hic blanditiis iusto nesciunt recusandae debitis
-    culpa sed veniam distinctio. Quod sequi eum iusto maxime autem nam nihil quis fuga
-    velit nobis eveniet molestiae quisquam, earum illum quos culpa expedita voluptatem
-    minima nostrum amet ad nisi. Amet aperiam in eum minima dolorum suscipit? Deleniti,
-    reprehenderit vitae alias corrupti voluptate dicta eius illum, minus qui
-    necessitatibus repellendus illo commodi odio aperiam quasi tempora cumque sit
-    similique exercitationem itaque tempore totam id sint! Tempore debitis laborum
-    obcaecati excepturi optio ad eaque dignissimos voluptate eum repellendus molestiae
-    corporis nulla, cupiditate ea temporibus quae omnis sequi quod nesciunt pariatur iusto
-    quis, molestias similique eveniet. Saepe exercitationem, sit pariatur debitis voluptas
-    ratione. Eum quam iusto a eligendi facere minima nam. Maiores non mollitia iure
-    commodi officiis, laboriosam vero quibusdam dolorem adipisci cupiditate, nobis neque,
-    id expedita ipsum similique pariatur nulla magnam exercitationem voluptate ab qui
-    beatae animi error. Consequatur odio, obcaecati deserunt inventore numquam eligendi.
-    Deserunt sit similique iste sunt fugit eius reiciendis, cumque cupiditate mollitia
-    officia ratione ipsa maiores alias, sed culpa non pariatur, perspiciatis eaque. Fugit
-    illo rem ipsum in, repellendus magnam molestias natus! Tempora, minima architecto iure
-    doloremque qui expedita officiis cumque similique distinctio totam! Quas quibusdam sit
-    recusandae aut suscipit expedita consectetur a facilis rem porro velit sunt sapiente
-    molestiae excepturi nisi, aperiam at necessitatibus esse commodi veritatis, culpa
-    ipsum, alias doloremque corporis? Expedita rem quae voluptatem illo aut voluptates hic
-    numquam quod quas aperiam. Rerum, non. Eveniet in optio eius dolorum ad eaque alias
-    fugiat, voluptatem libero! Harum suscipit quasi corporis facilis explicabo facere
-    veniam recusandae magni voluptatibus vel in asperiores ipsa alias tempore, nihil,
-    illum aperiam. Suscipit nobis praesentium rem sequi voluptates velit id, iste
-    repellendus consequatur possimus ullam numquam aliquid, iusto laborum expedita sit.
-    Voluptatibus, maiores. Alias praesentium, officia nam ipsa sunt suscipit itaque vel
-    illum provident! Reiciendis nobis voluptate culpa dicta, modi voluptates nemo dolorem,
-    illum rem at ad incidunt magnam beatae. Saepe molestias pariatur quasi quam optio.
-    Voluptatem aspernatur natus, rerum ea quod aliquam, quo quam amet odit, officiis
-    dignissimos error eius? Consectetur laboriosam veritatis animi, vel quidem nesciunt
-    sunt dolor dolore, cumque ipsa, quia magni dolores id. Repudiandae debitis deserunt,
-    corrupti quidem quisquam perferendis iste? Nisi alias facilis sequi doloremque.
-    Voluptas, itaque? Exercitationem, odit. A odit architecto iusto velit dolorum aliquid
-    nihil corrupti? Dolor doloribus delectus corporis qui molestiae ea cum aspernatur
-    laborum quam ipsa. Ea ipsa consequuntur explicabo rem? Itaque provident quos
-    necessitatibus reprehenderit recusandae sunt labore omnis illo suscipit ullam
-    molestias praesentium assumenda corrupti pariatur, illum tenetur in, a deleniti animi
-    ab corporis eveniet est perferendis ex. Dolorum mollitia impedit magni? Maxime laborum
-    cum eum, libero dicta consectetur voluptatum nostrum dolore culpa eius quasi
-    voluptatem fugiat ipsam nemo! Sequi blanditiis sit porro quis repudiandae recusandae
-    atque officiis magnam tenetur, laborum deserunt dolores fuga quidem asperiores, quam
-    maxime. Impedit corporis suscipit doloremque, sit adipisci repellendus sed. Sunt,
-    possimus consectetur eveniet natus ut quaerat placeat laudantium quo alias saepe nam
-    libero est reiciendis adipisci voluptatum eum ullam iste doloribus recusandae quam
-    excepturi! Odio cumque tempora doloremque architecto. Fugit, quod minus sint
-    repellendus eos quas? Voluptatem ad suscipit id, ea eos, dignissimos voluptates,
-    beatae praesentium iste velit quas doloribus. Nam quidem corporis cupiditate quam
-    voluptate quisquam quos, beatae ex explicabo exercitationem autem minima blanditiis,
-    odio vitae cum neque quibusdam veniam voluptates ipsa culpa! Neque reiciendis libero
-    facere dolorum a rem soluta, rerum consequatur provident vero possimus consectetur
-    delectus perferendis accusantium nostrum, molestias eveniet dignissimos architecto
-    consequuntur perspiciatis dolorem cupiditate? Est facere ducimus reiciendis.
-    Praesentium dolorem tempore numquam eaque repudiandae enim tempora debitis, fugit
-    asperiores molestiae quam, perferendis sapiente nesciunt modi a accusantium quis
-    laboriosam, odit placeat voluptas eum! Tempore eius voluptates fugit quasi libero
-    magnam voluptatibus maxime harum itaque, ea numquam, praesentium consequuntur quidem
-    a. Officiis numquam atque quidem, totam harum ipsum quisquam dolor sint cupiditate aut
-    quae quaerat nihil, labore nostrum reprehenderit porro dolores provident. Voluptate
-    natus a dolorem cum ad, quidem, eaque voluptas deleniti, rem dolore vero laborum.
-    Facilis inventore cumque, numquam quaerat itaque, recusandae nesciunt enim veniam
-    voluptatibus distinctio esse? Dolore rerum voluptas, nulla quia explicabo numquam cum
-    pariatur? Molestiae cumque sunt, culpa necessitatibus aut optio accusantium, alias
-    voluptas eius qui fuga quos rem. Pariatur animi assumenda cum dolorum culpa
-    praesentium cumque. Impedit iusto laboriosam consectetur ea dolor deserunt amet quis
-    ad, assumenda dolorum ducimus similique fugiat itaque praesentium consequatur dolorem
-    autem id optio quidem vero, officia accusantium animi esse! Amet, praesentium ipsa
-    officia doloribus, corporis itaque modi, ea quis dolorem provident consequuntur quasi?
-    Et laudantium beatae, delectus aut earum, consectetur id quia perferendis quis in
-    atque ab deleniti quam tempora eveniet.
-  </h1>
+  <Row gutter={[16, 16]}>
+    <Col span={6}>
+      <Card>
+        <Statistic
+          title="Active"
+          value={11.28}
+          precision={2}
+          valueStyle={{ color: '#3f8600' }}
+          prefix={<ArrowUpOutlined />}
+          suffix="%"
+        />
+      </Card>
+    </Col>
+    <Col span={6}>
+      <Card>
+        <Statistic
+          title="Idle"
+          value={9.3}
+          precision={2}
+          valueStyle={{ color: '#cf1322' }}
+          prefix={<ArrowDownOutlined />}
+          suffix="%"
+        />
+      </Card>
+    </Col>
+    <Col span={6}>
+      <Card>
+        <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
+      </Card>
+    </Col>
+    <Col span={6}>
+      <Card>
+        <Statistic title="Unmerged" value={93} suffix="/ 100" />
+      </Card>
+    </Col>
+    <Col span={16}>
+      <Card style={{ height: '500px' }}>
+        <Table columns={columns} dataSource={dataTable} />
+      </Card>
+    </Col>
+    <Col span={8}>
+      <Card style={{ height: '500px' }} className="h-full overflow-scroll">
+        <List
+          itemLayout="horizontal"
+          dataSource={dataList}
+          renderItem={(item) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title={<a href="https://ant.design">{item.title}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              />
+            </List.Item>
+          )}
+        />
+      </Card>
+    </Col>
+  </Row>
 );
 
 export default Dashboard;
