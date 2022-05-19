539928d73da131df8875e0b8257f8cbdf6b32ef6
rnwathon <rakhmatnashrul@gmail.com>
2022-05-19 15:19:35 +0700

    featL export forms, views, and feedbacks pages

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,1 +1,4 @@
 export { default as Dashboard } from './Dashboard';
+export { default as Forms } from './Forms';
+export { default as Views } from './Views';
+export { default as Feedbacks } from './Feedbacks';
