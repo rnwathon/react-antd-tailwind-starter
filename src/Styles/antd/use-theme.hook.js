830964e71f9b7c1b752ad8bb36891e62278849c6
rnwathon <rakhmatnashrul@gmail.com>
2022-05-21 02:24:41 +0700

    feat: create use-theme hook

---
+++ src/Styles/antd/use-theme.hook.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const DARK_MODE = 'dark-mode';
+
+const getDarkMode = () => JSON.parse(localStorage.getItem(DARK_MODE)) || false;
+
+export const useTheme = () => {
+  const [darkMode, setDarkMode] = useState(getDarkMode);
+
+  useEffect(() => {
+    const initialValue = getDarkMode();
+    if (initialValue !== darkMode) {
+      localStorage.setItem(DARK_MODE, darkMode);
+      window.location.reload();
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => setDarkMode((prevState) => !prevState);
+
+  return [darkMode, toggleDarkMode];
+};
