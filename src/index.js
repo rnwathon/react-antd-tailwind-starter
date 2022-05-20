dda2e2a184bb30a118e8ca0bf0b270a96f8eb109
rnwathon <rakhmatnashrul@gmail.com>
2022-05-21 02:34:04 +0700

    feat: integrate theme provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './Styles/antd/index.less';
-import './Styles/tailwind.css';
+import { ThemeProvider } from './Styles/antd/ThemeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
