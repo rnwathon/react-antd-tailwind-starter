7438b22420f6af8ab3867d557dad5f211839d54e
rnwathon <rakhmatnashrul@gmail.com>
2022-05-21 07:01:38 +0700

    feat: export auth pages and 404 page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,6 @@
+export * from './Auth';
+export { default as Page404 } from './404';
+
 export { default as Dashboard } from './Dashboard';
 export { default as Forms } from './Forms';
 export { default as Views } from './Views';
