9ffe2c33b476c37e9fd8105638919a8277ae1535
rnwathon <rakhmatnashrul@gmail.com>
2022-05-21 07:02:18 +0700

    feat: create get public route function

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,4 +1,54 @@
-import { Dashboard, Feedbacks, Forms, Views } from '../Pages';
+import {
+  //
+  Page404,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  //
+  Dashboard,
+  Feedbacks,
+  Forms,
+  Views,
+} from '../Pages';
+
+export const getPublicRoutes = () => [
+  {
+    path: '/login',
+    isIndex: false,
+    isCaseSensitive: false,
+    isDisabled: false,
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    isIndex: false,
+    isCaseSensitive: false,
+    isDisabled: false,
+    element: <Register />,
+  },
+  {
+    path: '/forgotPassword',
+    isIndex: false,
+    isCaseSensitive: false,
+    isDisabled: false,
+    element: <ForgotPassword />,
+  },
+  {
+    path: '/resetPassword',
+    isIndex: false,
+    isCaseSensitive: false,
+    isDisabled: false,
+    element: <ResetPassword />,
+  },
+  {
+    path: '*',
+    isIndex: false,
+    isCaseSensitive: false,
+    isDisabled: false,
+    element: <Page404 />,
+  },
+];
 
 export const getDashboardRoutes = () => [
   {
