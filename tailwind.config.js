1dca51f16c0c7e091c05e040440c28281ef6e362
rnwathon <rakhmatnashrul@gmail.com>
2022-05-22 20:18:34 +0700

    feat: extend tailwind screen breakpoint to match antd

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
+      screens: {
+        xs: '480px',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1600px',
+      },
       colors: {
         header: {
           light: '#ffffff',
