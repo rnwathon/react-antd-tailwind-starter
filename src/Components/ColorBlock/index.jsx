8fad38cebe185091b82f87b1b0057811652a4ed7
rnwathon <rakhmatnashrul@gmail.com>
2022-05-27 18:25:43 +0700

    fix: flex className duplicate in color block

--- src/Components/ColorBlock/index.jsx
+++ src/Components/ColorBlock/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ColorBlock = ({ children, className, id, ...props }) => {
-  const style = `p-2 flex grow shrink items-center justify-center ${className}`;
+  const style = `p-2 flex grow shrink ${className}`;
 
   return (
     <div id={id} className={style} {...props}>
