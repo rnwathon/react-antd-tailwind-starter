1f0ea6532ff4d0644d4a8aa76c798a244a878466
rnwathon <rakhmatnashrul@gmail.com>
2022-05-27 17:50:32 +0700

    feat: update color block component layout

--- src/Components/ColorBlock/index.jsx
+++ src/Components/ColorBlock/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ColorBlock = ({ children, className, ...props }) => {
+const ColorBlock = ({ children, className, id, ...props }) => {
   const style = `p-2 flex grow shrink items-center justify-center ${className}`;
 
   return (
-    <div className={style} {...props}>
+    <div id={id} className={style} {...props}>
       {children}
     </div>
   );
 };
 
 const ColorBlockGroup = ({ children, className, ...props }) => {
-  const style = `w-full flex flex-wrap  ${className}`;
+  const style = `w-full flex flex-wrap flex-col ${className}`;
   return (
     <div className={style} {...props}>
       {children}
